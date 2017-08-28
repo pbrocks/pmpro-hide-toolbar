56d17867ace0694f1e2434c86d0d034b45255960
Paul Barthmaier <paul@barthmaier.rocks>
2017-08-27 20:16:58 -0400

    Changing function names to Toolbar

--- inc/class-hide-adminbar-customizer.php
+++ inc/class-hide-adminbar-customizer.php
@@ -2,13 +2,13 @@
 
 defined( 'ABSPATH' ) || die( 'File cannot be accessed directly' );
 
-new Hide_Adminbar_Customizer();
+new Hide_Toolbar_Customizer();
 
-class Hide_Adminbar_Customizer {
+class Hide_Toolbar_Customizer {
 
 	public function __construct() {
 		add_action( 'customize_register', array( $this, 'site_customizer_manager' ) );
-		add_action( 'init', array( $this, 'disable_adminbar_on_frontend' ), 9 );
+		add_action( 'init', array( $this, 'disable_toolbar_on_frontend' ), 9 );
 	}
 
 	/**
@@ -16,31 +16,31 @@ class Hide_Adminbar_Customizer {
 	 *
 	 * @see add_action('customize_preview_init',$func)
 	 */
-	public function hide_admin_bar_in_edit_user_settings() {
+	public function hide_toolbar_in_edit_user_settings() {
 		$current_screen = get_current_screen();
 		if ( 'profile' === $current_screen->id || 'user-edit' === $current_screen->id) {
 			wp_enqueue_style( 'hide-profile-toolbar-option', plugins_url( '/css/hide-profile-toolbar-option.css', __FILE__ ) );
 		}
 	}
 	/**
-	 * [disable_adminbar_on_frontend description]
+	 * [disable_toolbar_on_frontend description]
 	 *
 	 * @param  [type] [description]
 	 * @return [type]             [description]
 	 */
-	public function disable_adminbar_on_frontend() {
-		if ( ! current_user_can( get_option( 'minimum_user_role' ) ) && '1' !== get_option( 'show_admin_bar_on_frontend' ) ) {
-			add_filter( 'show_admin_bar', '__return_false' );
-			add_action( 'admin_enqueue_scripts', 'hide_admin_bar_in_edit_user_settings' );
+	public function disable_toolbar_on_frontend() {
+		if ( ! current_user_can( get_option( 'minimum_user_role' ) ) && '1' !== get_option( 'show_toolbar_on_frontend' ) ) {
+			add_filter( 'show_toolbar', '__return_false' );
+			add_action( 'admin_enqueue_scripts', 'hide_toolbar_in_edit_user_settings' );
 		}
 	}
 	/**
-	 * [disable_adminbar_on_frontend description]
+	 * [disable_toolbar_on_frontend description]
 	 *
 	 * @param  [type] [description]
 	 * @return [type]             [description]
 	 */
-	public function dropdown_for_adminbar_user_role() {
+	public function dropdown_for_toolbar_user_role() {
 		$dropdown = wp_dropdown_roles( $selected );
 		return $dropdown;
 	}
@@ -52,21 +52,41 @@ class Hide_Adminbar_Customizer {
 	 * @return [type]             [description]
 	 */
 	public function site_customizer_manager( $customizer_additions ) {
-		$this->panel_creation( $customizer_additions );
+		$this->create_panel( $customizer_additions );
+		$this->create_section( $customizer_additions );
 	}
 
 	/**
-	 * A section to show how you use the default customizer controls in WordPress
+	 * Customizer Section
 	 *
 	 * @param Obj $customizer_additions - WP Manager
 	 *
 	 * @return Void
 	 */
-	private function panel_creation( $customizer_additions ) {
-		$customizer_additions->add_section( 'mysite_section', array(
-			'title'          => 'MySite Custom Controls',
-			'description'    => 'MySite Custom Controls was created specifically to be included in the Hide Adminbar Plugin, but likely won\'t be used.',
+	private function create_panel( $customizer_additions ) {
+		$customizer_additions->add_panel(
+			'pmpro_panel', array(
+			'title'       => 'PMPro Panel',
+			'label'       => 'PMPro Panel',
+			'description' => 'This is a description of this PMPro panel',
+			'priority'    => 10,
+			)
+		);
+	}
+
+	/**
+	 * Customizer Section
+	 *
+	 * @param Obj $customizer_additions - WP Manager
+	 *
+	 * @return Void
+	 */
+	private function create_section( $customizer_additions ) {
+		$customizer_additions->add_section( 'hide_toolbar_section', array(
+			'title'          => 'PMPro Hide Toolbar',
+			'description'    => 'PMPro Hide Toolbar was created specifically to be included in the Hide Toolbar Plugin, but likely won\'t be used.',
 			'priority'       => 35,
+			'panel'          => 'pmpro_panel',
 		) );
 
 		/**
@@ -76,20 +96,20 @@ class Hide_Adminbar_Customizer {
 			require_once dirname( __FILE__ ) . '/controls/checkbox/toggle-control.php';
 		}
 
-		$customizer_additions->add_setting( 'show_admin_bar_on_frontend', array(
+		$customizer_additions->add_setting( 'show_toolbar_on_frontend', array(
 			'default'        => true,
 			'type'           => 'option',
 			// 'type'           => 'theme_mod',
 			'transport'      => 'refresh',
 		) );
 
-		$customizer_additions->add_control( new Customizer_Toggle_Control( $customizer_additions,'show_admin_bar_on_frontend', array(
-				'label'   => 'Toggle Frontend Adminbar',
-				'section' => 'mysite_section',
-				'settings'   => 'show_admin_bar_on_frontend',
+		$customizer_additions->add_control( new Customizer_Toggle_Control( $customizer_additions,'show_toolbar_on_frontend', array(
+				'label'   => 'Toggle Frontend Toolbar',
+				'section' => 'hide_toolbar_section',
+				'settings'   => 'show_toolbar_on_frontend',
 				// 'type'    => 'checkbox',
 				'type'    => 'ios',
-				'description'   => 'Toggle on or off the Frontend Adminbar for non-admins. Toggle is equivalent to a checkbox.',
+				'description'   => 'Toggle on or off the Frontend Toolbar for non-admins. Toggle is equivalent to a checkbox.',
 				'priority' => 10,
 				)
 		) );
@@ -121,7 +141,7 @@ class Hide_Adminbar_Customizer {
 			    'label'      => __( 'Select User Role' ), 
 			    'description' => __( 'Using this option you can change the theme colors' ),
 			    'priority'   => 10,
-				'section' => 'mysite_section',
+				'section' => 'hide_toolbar_section',
 			    'type'    => 'select',
 			    'choices' => $user_role_choices,
 			)
